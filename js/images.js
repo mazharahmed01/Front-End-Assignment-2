2537bbe9a565006c249dd2a9832a6d7ee99f17ad
mazharahmed01 <mazhar_ahmed@live.com>
2017-12-17 02:25:05 +0000

    added image style to drivers page

---
+++ js/images.js
@@ -0,0 +1,31 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+
+window.openModal = function(img) {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var images = document.querySelectorAll('img');
+for(var i=0, len = images.length; i < len; i++){
+    images[i].addEventListener('click', openModal);
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+
+
+
